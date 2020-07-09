dbb070b2aa7a1ef5bd30dbde3b6502e0077191fb
Ericec <wwwzhouliang@gmail.com>
2020-07-09 18:06:23 +0800

    注释 footer 底部一言随机内容。注释移动端 footer 显示友情链接

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
     <div class="container">
 
 		<!-- 一言开始 -->
-		<p id="hitokoto">:D 获取中...</p>
+<!-- 		<p id="hitokoto">:D 获取中...</p>
 <script>
   fetch('https://v1.hitokoto.cn')
     .then(function (res){
@@ -19,17 +19,17 @@
     .catch(function (err) {
       console.error(err);
     })
-</script>
+</script> -->
 		<!-- 一言结束 -->
-	<?php if (wp_is_mobile()){ ?>
+<!-- 	<?php if (wp_is_mobile()){ ?>
 	<div class="widget-links">
 		<ul>
 			<li>友情链接:<?php wp_list_bookmarks('title_li=&categorize=0'); ?></li>	
 		</ul>
 	</div>
     <?php } else { ?>
-    <?php } ?>
-        <p>&copy; Copyright 2018-2019 <a href="/"><?php bloginfo( 'name' ); ?></a> All Rights Reserved. Powered by <a href="https://wordpress.org/" target="_blank">WordPress</a></p> 
+    <?php } ?> -->
+        <p>&copy; Copyright 2016-2020 <a href="/"><?php bloginfo( 'name' ); ?></a> All Rights Reserved. Powered by <a href="https://www.aips.me/" target="_blank">WordPress</a></p> 
 
 
         <p>&nbsp</p>
